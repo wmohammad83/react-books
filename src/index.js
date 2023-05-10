0c3acd2466a0a725b7f13acbe14767b101e3cea5
Waseem Mohammad <waseemmohammad@Waseems-MacBook-Pro.local>
2023-05-10 22:05:45 +0100

    useContext used instead of props to pass data down and custom hook created to reduce to create dry code

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import {Provider} from "./context/books";
 
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
 
-const el = document.getElementById("root")
-const root = ReactDOM.createRoot(el)
-
-root.render(<App />)
+root.render(
+  <Provider >
+    <App />
+  </Provider>
+);
